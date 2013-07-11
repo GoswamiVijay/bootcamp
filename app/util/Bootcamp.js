ab3e1c3124feca6522bdf8016f6796370a9fb220
Patrick Chu <patrick.chu@sencha.com>
2013-07-10 23:42:10 -0400

    removed debugger statement from Bootcamp.js

--- app/util/Bootcamp.js
+++ app/util/Bootcamp.js
@@ -24,7 +24,6 @@ Ext.define('BucketList.util.Bootcamp', {
 
 		// get a reference to the "navigationview" xtype which contains this dataview
 		var nv = dataview.up('navigationview');
-		debugger;
 
 		// push the screen with the xtype "categoryitems" into the 
 		//		navigation view
